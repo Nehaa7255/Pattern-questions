86c388949b9029dd03cef0a3ea322e0b673cb753
Nehaa7255 <nehamas04@gmail.com>
2024-03-27 22:49:29 +0530

    Pattern question added

--- src/sixteen.java
+++ src/sixteen.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
 public class sixteen {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int num=5;
 
-    
+        for(int i=0;i<num;i++)
+        {
+            for(char j=0;j<=i;j++){
+                System.out.print((char)((int)('A'+i)) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
