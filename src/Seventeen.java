0801c8e6799ecb5f8d9dfe3ddc1a7aecf1316240
Nehaa7255 <nehamas04@gmail.com>
2024-03-28 23:01:02 +0530

    Pattern question added

--- src/Seventeen.java
+++ src/Seventeen.java
@@ -3,19 +3,16 @@ public class Seventeen {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int num=5;
-        for(int i=0;i<num;i++){
-            for(char j=0;j<num-i-1;j++) {
+        for(int i=1;i<num;i++){
+            for(int j =1;j<num-i+1;j++) {
                 System.out.print(" ");
             }
-
-           for(char j='A';j<='A'+i;j++){
-               System.out.print(j);
+           for(int k=1;k<=i;k++){
+               System.out.print(k);
            }
            for(char j=2;j>='A';j--){
                System.out.print(j);
             }
-
-
             System.out.println();
         }
     }
--- src/Twelve.java
+++ src/Twelve.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
 public class Twelve {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=4;
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j <= i) {
+                    System.out.print(j);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = n; j > 0; j--) {
+                if (j <= i) {
+                    System.out.print(j);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
-
 }
+
