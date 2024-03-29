161159ae295fa46e447a188c303bd608c01329e2
Nehaa7255 <nehamas04@gmail.com>
2024-03-29 23:09:30 +0530

    Pattern question added

--- src/Seventeen.java
+++ src/Seventeen.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
 public class Seventeen {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int num=5;
-        for(int i=1;i<num;i++){
-            for(int j =1;j<num-i+1;j++) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N= 5;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
                 System.out.print(" ");
             }
-           for(int k=1;k<=i;k++){
-               System.out.print(k);
-           }
-           for(char j=2;j>='A';j--){
-               System.out.print(j);
+            char ch = 'A';
+            int breakpoint = (2 * i + 1) / 2;
+            for (int j = 1; j <= 2 * i + 1; j++) {
+                System.out.print(ch);
+                if (j <= breakpoint) ch++;
+                else ch--;
+            }
+            for (int j = 0; j < N - i - 1; j++) {
+                System.out.print(" ");
             }
             System.out.println();
         }
     }
-    }
+}
